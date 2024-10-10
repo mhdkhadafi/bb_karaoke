31fe83f24fdfff65643a6724642c03a644e14a46
EC2 Default User <ec2-user@ip-172-31-39-144.ec2.internal>
2024-10-10 11:15:33 +0000

    cookie

--- web/karaoke_video_maker.py
+++ web/karaoke_video_maker.py
@@ -11,6 +11,7 @@ from app_db import update_progress
 import requests
 from celery_app import app as celery_app
 import time
+import shutil
 
 def rename_file_without_special_chars(file_path):
     # Replace problematic characters (e.g., single quotes)
@@ -23,49 +24,59 @@ def remove_audio(video_path):
     video_without_audio = video.without_audio()
     return video_without_audio
 
-def remove_vocals(audio_path, accompaniment_file):
-    spleeter_api_url = "http://spleeter:5001/split"
-    response = requests.post(spleeter_api_url, json={"file_path": audio_path, "accompaniment_file": accompaniment_file})
+#def remove_vocals(audio_path, accompaniment_file):
+#    spleeter_api_url = "http://spleeter:5001/split"
+#    response = requests.post(spleeter_api_url, json={"file_path": audio_path, "accompaniment_file": accompaniment_file})
+#
+#    if response.status_code == 200:
+#        print("Spleeter separation successful! Waiting for the output file...")
+#        
+#        # Wait for the accompaniment file to be created (polling)
+#        for _ in range(60):  # Wait for up to 60 seconds
+#            if os.path.exists(accompaniment_file):
+#                return accompaniment_file
+#            time.sleep(1)  # Wait for 1 second before checking again
+#        
+#        raise Exception(f"Accompaniment file not found: {accompaniment_file}")
+#    else:
+#        raise Exception(f"Spleeter error: {response.json().get('error')}")
 
-    if response.status_code == 200:
-        print("Spleeter separation successful! Waiting for the output file...")
-        
-        # Wait for the accompaniment file to be created (polling)
-        for _ in range(60):  # Wait for up to 60 seconds
-            if os.path.exists(accompaniment_file):
-                return accompaniment_file
-            time.sleep(1)  # Wait for 1 second before checking again
-        
-        raise Exception(f"Accompaniment file not found: {accompaniment_file}")
-    else:
-        raise Exception(f"Spleeter error: {response.json().get('error')}")
-
-def remove_vocals_2(input_file, output_folder):
+def remove_vocals(input_file, output_folder):
+    volume_path = "/home/ec2-user/bb_karaoke/shared"
     command = [
-        "docker", "run", "--rm",
-        "-v", f"{input_file}:/app/input",
-        "-v", f"{output_folder}:/app/output",
-        "deezer/spleeter", "separate", "-i", "/app/input", "-o", "/app/output"
+        "docker", "run", 
+        "-v", f"{volume_path}:/shared",
+        "deezer/spleeter:3.8", 
+        "separate", 
+        "-o", output_folder, 
+        input_file
     ]
+
     subprocess.run(command, check=True)
 
+
 # Step 2: Add .ogg audio to the video
-def add_audio(video_file, audio_with_vocals, audio_without_vocals, output_file):
+def add_audio(video_file, audio_without_vocals, output_file):
+    #command = [
+    #    'ffmpeg',
+    #    '-i', video_file,                     # Input video (VP8 in WebM)
+    #    '-i', audio_with_vocals,              # First audio track (with vocals)
+    #    '-i', audio_without_vocals,           # Second audio track (without vocals)
+    #    '-map', '0:v',                        # Map video stream
+    #    '-map', '1:a',                        # Map first audio stream
+    #    '-map', '2:a',                        # Map second audio stream
+    #    '-c:v', 'libx264',                    # Re-encode video to H.264 for MP4 compatibility
+    #    '-c:a', 'aac',                        # Encode audio to AAC
+    #    '-strict', 'experimental',            # Enable experimental features for AAC
+    #    '-shortest',                          # Ensure video and audio match length
+    #    '-preset', 'ultrafast',               # Optionally speed up encoding (can change to 'fast', etc.)
+    #    output_file                           # Output file
+    #]
     command = [
-        'ffmpeg',
-        '-i', video_file,                     # Input video
-        '-i', audio_with_vocals,              # First audio track (with vocals)
-        '-i', audio_without_vocals,           # Second audio track (without vocals)
-        '-map', '0:v',                        # Map video stream
-        '-map', '1:a',                        # Map first audio stream
-        '-map', '2:a',                        # Map second audio stream
-        '-c:v', 'copy',                       # Copy video stream without re-encoding
-        '-c:a', 'aac',                        # Encode audio to AAC
-        '-strict', 'experimental',            # Enable experimental features for aac
-        '-shortest',                          # Make sure the video and audio streams match length
-        output_file                           # Output file
+        'ffmpeg', '-i', video_file, '-i', audio_without_vocals, 
+        '-c:v', 'libx264', '-preset', 'ultrafast', '-c:a', 'aac', '-strict', 'experimental', '-map', '0:v', '-map', '1:a', output_file
     ]
-    
+
     subprocess.run(command, check=True)
 
 # Step 3: Convert .lrc file to .srt for subtitle embedding
@@ -131,7 +142,7 @@ def embed_subtitles(video_path, main_srt, after_srt, output_path, song_name, art
     font = "Arial Unicode MS"
     command_main = [
         'ffmpeg', '-i', video_path, '-vf', 
-        f"subtitles={main_srt}:force_style='Fontname={font},Fontsize=24,PrimaryColour=&H00FFFFFF,OutlineColour=&H00000000,BorderStyle=3,Alignment=2,MarginV=30'",
+        f"subtitles='{main_srt}':force_style='Fontname={font},Fontsize=24,PrimaryColour=&H00FFFFFF,OutlineColour=&H00000000,BorderStyle=3,Alignment=2,MarginV=30'",
         temp_output_1
     ]
     subprocess.run(command_main)
@@ -139,7 +150,7 @@ def embed_subtitles(video_path, main_srt, after_srt, output_path, song_name, art
     update_progress(song_name, artist_name, album_name, 9, "Embedding Subtitles - After")
     command_after = [
         'ffmpeg', '-i', temp_output_1, '-vf', 
-        f"subtitles={after_srt}:force_style='Fontname={font},Fontsize=18,PrimaryColour=&H00808080,OutlineColour=&H00000000,BorderStyle=3,Alignment=2,MarginV=10'",
+        f"subtitles='{after_srt}':force_style='Fontname={font},Fontsize=18,PrimaryColour=&H00808080,OutlineColour=&H00000000,BorderStyle=3,Alignment=2,MarginV=10'",
         output_path
     ]
     subprocess.run(command_after)
@@ -171,12 +182,13 @@ def create_karaoke(artist_name, album_name, song_name):
         raise FileNotFoundError("One or more input files are missing (webm, ogg, lrc).")
 
     # Generate unique temporary filenames based on artist, album, and song name
-    temp_video_no_audio = f"{artist_name}_{album_name}_{song_name}_temp_no_audio.webm"
-    temp_video_subtitles = f"{artist_name}_{album_name}_{song_name}_temp_subtitles.mp4"
-    temp_srt_file = f"{artist_name}_{album_name}_{song_name}_subtitles.srt"
-    temp_main_srt_file = rename_file_without_special_chars(f"{artist_name}_{album_name}_{song_name}_main.srt")
-    temp_after_srt_file = rename_file_without_special_chars(f"{artist_name}_{album_name}_{song_name}_after.srt")
-    temp_accompaniment_file = os.path.join(output_folder, f"{artist_name}_{album_name}_{song_name}_accompaniment.wav")
+    temp_video_no_audio = os.path.join(input_folder, f"{artist_name}_{album_name}_{song_name}_temp_no_audio.webm")
+    temp_video_subtitles = os.path.join(input_folder, f"{artist_name}_{album_name}_{song_name}_temp_subtitles.mp4")
+    temp_srt_file = os.path.join(input_folder, f"{artist_name}_{album_name}_{song_name}_subtitles.srt")
+    temp_main_srt_file = os.path.join(input_folder, kvm.rename_file_without_special_chars(f"{artist_name}_{album_name}_{song_name}_main.srt"))
+    temp_after_srt_file = os.path.join(input_folder, kvm.rename_file_without_special_chars(f"{artist_name}_{album_name}_{song_name}_after.srt"))
+    temp_accompaniment_file = os.path.join(input_folder, f"{artist_name}_{album_name}_{song_name}_accompaniment.wav")
+    original_accompaniment_file = os.path.join(input_folder, f"{artist_name} - {song_name}", 'accompaniment.wav')
 
     # Step 1: Remove audio from video
     update_progress(song_name, artist_name, album_name, 4, "Removing Audio")
@@ -185,8 +197,10 @@ def create_karaoke(artist_name, album_name, song_name):
 
     # Step 2: Add .ogg audio to video
     update_progress(song_name, artist_name, album_name, 5, "Adding Audio")
-    remove_vocals(audio_file, temp_accompaniment_file)
-    add_audio(temp_video_no_audio, audio_file, temp_accompaniment_file, temp_video_subtitles)
+    remove_vocals(audio_file, input_folder)
+    shutil.move(original_accompaniment_file, temp_accompaniment_file)
+    # add_audio(temp_video_no_audio, audio_file, temp_accompaniment_file, temp_video_subtitles)
+    add_audio(temp_video_no_audio, temp_accompaniment_file, temp_video_subtitles)
 
     # Step 3: Convert .lrc to .srt
     update_progress(song_name, artist_name, album_name, 6, "Converting Lyrics to SRT")
